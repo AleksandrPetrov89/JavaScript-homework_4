5022d6c637bfcc9e2f35fe60d0726ad63b92c3de
AleksandrPetrov89 <petrovav89@gmail.com>
2023-06-19 23:53:15 +0500

    Task 3 Guess number with async/await repaired

--- task_3/guess_number_async.js
+++ task_3/guess_number_async.js
@@ -11,43 +11,46 @@ const questionPromise = function (query, options) {
 };
 
 
-async function guessNumber (number, log = "", counter = 1) {
+async function guessNumber (number) {
     /* Игра "угадай число" с подсказками "больше" и "меньше".
         number - число, которое необходимо отгадать, может быть от 0 до 999;
         log - необязательный параметр, строка, которая выведется вначале протокола записи игры
         counter - необязательный параметр, число. Точка отсчета попыток выиграть. */
-    let check = await questionPromise (`Попытка № ${counter}. \nВведите число от 0 до 999:\n`);
-    let message = `Попытка № ${counter}. \nВведите число от 0 до 999: \nВы ввели: ${check}\n`;
-    log += message;
-    if (check === "q") {
-        message = "\nВы вышли из игры!";
-        console.log(message + "\n");
-        rl.close();
+        log = "";
+        counter = 1;
+    while (true) {
+        let check = await questionPromise (`Попытка № ${counter}. \nВведите число от 0 до 999:\n`);
+        let message = `Попытка № ${counter}. \nВведите число от 0 до 999: \nВы ввели: ${check}\n`;
         log += message;
-        fs.appendFile("game_protocol.txt", log);
-        return;
+        if (check === "q") {
+            message = "\nВы вышли из игры!";
+            console.log(message + "\n");
+            rl.close();
+            log += message;
+            fs.appendFile("game_protocol.txt", log);
+            return;
+        };
+        check = Number(check);
+        if (isNaN(check) || check < 0 || check > 999) {
+            message = "Вы ошиблись при вводе!\n";
+        } else if (number === check) {
+            message = `\nВы угадали с ${counter} попытки! Поздравляю!!!`;
+            console.log(message + "\n");
+            rl.close();
+            log += message;
+            fs.appendFile("game_protocol.txt", log);
+            return;
+        } else if (number < check) {
+            message = "Загаданное число меньше, чем Вы предположили!\n";
+        } else if (number > check) {
+            message = "Загаданное число больше, чем Вы предположили!\n";
+        } else {
+            message = "\nКакая-то непонятная ошибка!!!\n";
+        };
+        console.log(message);
+        log += message + "\n";
+        counter += 1;
     };
-    check = Number(check);
-    if (isNaN(check) || check < 0 || check > 999) {
-        message = "Вы ошиблись при вводе!\n";
-    } else if (number === check) {
-        message = `\nВы угадали с ${counter} попытки! Поздравляю!!!`;
-        console.log(message + "\n");
-        rl.close();
-        log += message;
-        fs.appendFile("game_protocol.txt", log);
-        return;
-    } else if (number < check) {
-        message = "Загаданное число меньше, чем Вы предположили!\n";
-    } else if (number > check) {
-        message = "Загаданное число больше, чем Вы предположили!\n";
-    } else {
-        message = "\nКакая-то непонятная ошибка!!!\n";
-    };
-    console.log(message);
-    log += message + "\n";
-    counter += 1;
-    guessNumber (number, log, counter);
 };
 
 
